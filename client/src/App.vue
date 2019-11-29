39fcd206fa689125d2cfd2f1026019dcdbe77ea9
cjc <chenjuechong@sina.com>
2019-11-29 09:55:04 +0800

    the second

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import "fontawesome";
+
+export default {
+  name: 'App'
+}
+</script>
+
---
+++ client/src/components/adminList.vue
@@ -0,0 +1,140 @@
+import { getDragList } from '@/api/data';
+<template>
+    <div class="adminList main">
+        <div class="input_box">
+            <input v-model="Admin.name" type="text" class="myinput" placeholder="用户名" />
+            <input v-model="Admin.phone" type="text" class="myinput" placeholder="手机号"/>
+            <input v-if="!editAdminObj" v-model="Admin.password" class="myinput" type="password" placeholder="密码"> 
+            <button v-if="!editAdminObj" class="btn" @click="addAdmin()"><i class="fa fa-plus" aria-hidden="true"></i>添加</button>
+            <button v-if="editAdminObj" class="btn" @click="saveEditAdmin()"><i class="fa fa-save" aria-hidden="true"></i>保存</button>
+            <button style="opacity:0.8;" v-if="editAdminObj" class="btn" @click="cancelEditAdmin()"><i class="fa fa fa-times-circle-o" aria-hidden="true"></i>取消</button>
+        </div>
+        <grid
+            :listData="listData"
+			:theadData="theadData"
+			:ifEdit="true"
+			:ifDelete="true"
+			:ifpage="true"
+			:pageInfo="pageInfo"
+			@on-delete="deleteAdmin"
+			@on-edit="editAdmin"
+			@on-gopage="gopage">
+        </grid>
+    </div>
+</template>
+<script>
+var theadData = [
+    {
+        title:"用户名",
+        keyname:"name"
+    },{
+        title:"手机号",
+        keyname:"phone"
+    }
+];
+import grid from './grid.vue'
+export default {
+    name:'adminList',
+   data() {
+      return {
+          listData:[],
+          theadData:theadData,
+          Admin:{ //用户信息
+              name:"",
+              phone:"",
+              password:"",
+          },
+          editAdminObj:null,    //用于存放正在编辑的用户
+          pageInfo:{}
+      }
+   },
+   mounted:function(){
+       this.getAdminList(1);
+   },
+   methods: {
+       getAdminList(page){
+           var _this = this;
+
+           this.$reqs.post('/users/AdminList',{
+               page:page
+           }).then(function(result){
+               //成功
+               _this.listData = result.data.data;
+               _this.pageInfo.allpage = Math.ceil(result.data.total/5);
+           }).catch(function(error){
+               //失败
+               console.log(error);
+           });
+       },
+       addAdmin(){ //添加用户
+            if(!this.Admin.name || !this.Admin.phone || !this.Admin.password){
+                alert("不能为空");
+                return false;
+            }
+            this.$reqs.post('/users/add',this.Admin).then((result)=>{
+                //成功
+                this.getAdminList();
+                this.emptyAdmin();
+            }).catch(function(error){
+                //失败
+                console.log(error)
+            });
+       },
+       editAdmin(item){ //编辑用户
+            this.editAdminObj = item;
+            this.Admin = JSON.parse(JSON.stringify(item));
+       },
+       saveEditAdmin(){
+           if(!this.Admin.name || this.Admin.phone){
+               alert("不能为空");
+               return false;
+           }
+           this.$reqs.post('/users/update', this.Admin).then((result)=>{
+               //成功
+               this.gopage(this.pageInfo.currrent);
+
+               this.editAdminObj = null;
+               this.emptyAdmin();
+           }).catch(function(error){
+               //失败
+               console.log(error)
+           });
+       },
+       cancelEditAdmin(){
+           this.editAdminObj = null;
+           this.emptyAdmin();
+       },
+       deleteAdmin(item){
+           this.$reqs.post('/users/delete',item).then((result)=>{
+               //成功
+               console.log(result)
+               this.gopage(this.pageInfo.currrent);
+               this.emptyAdmin();
+           }).catch(function(error){
+               //失败
+               console.log(error)
+           });
+       },
+       gopage(index){
+           this.pageInfo.currrent = index;
+           //查询数据
+           this.getAdminList(index)
+       }
+
+   },
+   components:{grid}
+}
+</script>
+<style scoped>
+.main{
+    border-radius: 4px;
+    background: #fff;
+    margin-top: 10px;
+}
+.input_box{
+    padding: 0 10px;
+}
+.input_box .myinput{
+    width:25%;
+}
+</style>
